8f0637b108dad72b9c6cf1d9779b9402c4da0a11
Anastasia Garcia <anastasia.garcia@cooper.edu>
2021-09-08 18:55:43 -0400

    week 2 lab 1

---
+++ cs102/lab-2/lab2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ( int argc, char **argv )
+
+{
+
+/* for loops, assign variables, printing, everything else */
+
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+fprintf( stdout, "hello,world!\n" );
+
+return 0;
+}
