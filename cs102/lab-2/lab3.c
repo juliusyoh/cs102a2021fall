2191c70e7f00bd7e5b28f05bb0a4b63a4b6af225
Anastasia Garcia <anastasia.garcia@cooper.edu>
2021-09-08 19:12:29 -0400

    week 2 lab 3

---
+++ cs102/lab-2/lab3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ( int argc, char **argv )
+{
+
+int i = 1;
+
+while( i < 11 )
+
+{ 
+
+/* for loops, assign variables, printing, everything else */
+
+fprintf( stdout, "hello,world!\n" );
+i = i + 1;
+
+}
+
+return 0;
+}
