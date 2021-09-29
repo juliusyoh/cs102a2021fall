fac1d3745a512fb0b99ad5453c412b31ff464bd8
Anastasia Garcia <anastasia.garcia@cooper.edu>
2021-09-29 18:50:18 -0400

    lab1a milestone - done or whatever

---
+++ cs102/lab-5/lab1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ * Julius Yoh
+ * CS102 Fall 2021
+ * Lab 5 - Lab1.c
+ * 09-29-2021
+ */
+int main ( int argc, char **argv )
+/* This portion holds two sections that generate 5 ten times through allowing two "for" loops
+ {
+	int array[9], i;
+	for( i=0; i<10; i=i+1 ) 
+	{
+	array[i] = 1;
+	for (i = 0; i < 10; i = i +1)
+		{
+		fprintf(stdout,"%d\n",5);
+		}
+	}
+
+
+return 0;
+}
+	
