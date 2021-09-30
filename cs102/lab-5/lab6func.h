775bd9795ef51185f883f6d70d09f0ef68fbf316
Anastasia Garcia <anastasia.garcia@cooper.edu>
2021-09-29 22:31:45 -0400

    lab6funchstuff

---
+++ cs102/lab-5/lab6func.h
@@ -0,0 +1,12 @@
+
+/*
+ * Julius Yoh
+ * CS102 Fall 2021
+ * Lab 5 - Lab4.h
+ * 09-29-2021
+ */
+
+void fill( int matrix[5][5] );
+void print (int matrix[5][5] );
+void add(int matrix[5][5],int matrix1[5][5], int matrix2[5][5] );
+
