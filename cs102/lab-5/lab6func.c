d5d29794bbbefe2dc00c6da73cef720b3c590892
Anastasia Garcia <anastasia.garcia@cooper.edu>
2021-09-29 22:31:03 -0400

    lab6funcstuff

---
+++ cs102/lab-5/lab6func.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "lab6func.h"
+
+/*
+ * Julius Yoh
+ * CS102 Fall 2021
+ * Lab 5 - Lab4func.c
+ * 09-29-2021
+ */
+
+void fill (int matrix[5][5])
+ {
+int  i = 0;
+int j = 0;
+
+
+for( i=0; i<5; i=i+1 )
+      {
+	for( j = 0; j < 5; j = j + 1)
+		{
+                matrix[i][j] =  labs(mrand48()%100);
+  		}
+        }
+ }
+
+
+void print (int matrix[5][5])
+ {
+int  i = 0;
+int j = 0;
+
+
+	for (i = 0; i < 5; i = i +1)
+	{
+		for (j = 0; j < 5; j = j +1)
+		{
+			fprintf(stdout, "%5d", matrix[i][j]);
+			
+		}
+		fprintf(stdout, "\n" );
+	}
+ 
+ }
+
+void add (int matrix[5][5], int matrix1[5][5], int matrix2[5][5])
+{
+int i = 0;
+int j = 0;
+
+
+for( i=0; i<5; i=i+1 )
+      {
+        for( j = 0; j < 5; j = j + 1)
+                {
+                matrix2[i][j] = matrix[i][j]+ matrix1[i][j]; 
+                }
+        }
+ }
+
+
+
+
+
