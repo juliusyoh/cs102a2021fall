1880b3c7127a732c2773fca3835f39e46242d9cb
Anastasia Garcia <anastasia.garcia@cooper.edu>
2021-09-29 21:44:32 -0400

    lab5b

--- cs102/lab-5/lab5func.c
+++ cs102/lab-5/lab5func.c
@@ -16,21 +16,22 @@ void fill (int matrix[5][5])
 int  i = 0;
 int j = 0;
 
+
 for( i=0; i<5; i=i+1 )
-        {
-	for( j = 0; j < 5; j = j + 1){
+      {
+	for( j = 0; j < 5; j = j + 1)
+		{
                 matrix[i][j] =  labs(mrand48()%100);
   		}
         }
-
  }
 
+
 void print (int matrix[5][5])
-
-
  {
 int  i = 0;
 int j = 0;
+
 
 	for (i = 0; i < 5; i = i +1)
 	{
