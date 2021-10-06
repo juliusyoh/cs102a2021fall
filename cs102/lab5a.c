910288afe52f8417ff6f2eb8468ce521df902cb7
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-10-06 19:42:41 -0400

    Create lab5a.c

---
+++ cs102/lab5a.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int string_length(char* string);
+
+int main( int argc, char** argv )
+{
+int i = 1;
+
+if (argc > 1)
+	{
+        for(i = argc; i-- > 1; )                
+		{
+		int x = string_length(argv [i]);
+                fprintf( stdout, "%s %d \n", argv[i], x );
+                }
+
+	}
+
+else 
+	{
+	fprintf( stdout, "No arguments found!");
+	}
+
+  return 0;
+}
+int string_length(char* string)
+	{
+	return 42;
+	}
+
+
