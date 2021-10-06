9afa73726e175cca5e024e46cca63c71319f3067
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-10-06 17:43:32 -0400

    Create lab2c.c

---
+++ cs102/lab-6/lab2c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main( int argc, char** argv )
+{
+int i = 1;
+        for(i = 1; i < argc; i = i + 1)
+                {
+                fprintf( stdout, "%s\n", argv[i]);
+
+                }
+
+
+  return 0;
+}
+
