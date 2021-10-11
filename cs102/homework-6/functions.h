fd1dcffd755ce18f5d333229a4b26fb7f1225448
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-10-11 00:00:43 -0400

    Create functions.h

---
+++ cs102/homework-6/functions.h
@@ -0,0 +1,13 @@
+/*
+ * Julius Yoh
+ * CS102 Fall 2021
+ * Homework 6 - functions.h - Final Version
+ * October 10, 2021
+ */
+
+int string_length(char* string);
+void reverse_string( char* string);
+
+
+
+
