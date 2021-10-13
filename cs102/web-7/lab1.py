e8c7fa94e0a9aec284c62c72514cffa931c1489c
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-10-13 18:37:09 -0400

    Create lab1.py

---
+++ cs102/web-7/lab1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+print( "Content-type: text/html\n\n" )
+
+print( """
+<html>
+<body>
+""" )
+i = 0
+while (i < 101):
+                print("<h1>Hello, world</h1>")
+                i = i + 1
+
+print( """
+</body>
+</html>""" )
+
