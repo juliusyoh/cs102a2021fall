20582b939f06a17c0b011c493d606aac5d3bcb47
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-10-13 19:32:30 -0400

    Create lab6.c

---
+++ cs102/web-7/lab6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char**argv)
+        {
+int i = 1;
+while (i < 101)
+                {
+        printf("Hello World!");
+        fprintf(stdout, "\n" );
+        i = i + 1;
+                }
+
+        }
+
+
+
+
