f407aab07102bb1db467d06f27ea1b1feeb5b5e8
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-10-13 20:44:40 -0400

    Create functions.h
    here

---
+++ cs102/web-7/functions.h
@@ -0,0 +1,27 @@
+
+/*
+ * Julius Yoh
+ * CS102 Fall 2021
+ * Homework 7 - functions.h - Final Version
+ * October 13, 2021
+ */
+
+/* These fill the matrices through functions */
+
+
+void fill12x8 (int matrix1[12][8] );
+void fill8x12 (int matrix2[8][12] );
+
+/* This cross multiply the matrices */
+
+void multiply(int matrix1[12][8], int matrix2[8][12], int matrix3[12][12] );
+
+/* These print the matrices through functions */
+
+void print12x8 (int matrix1[12][8] );
+void print8x12 (int matrix2[8][12] );
+void print12x12 (int matrix3[12][12] );
+
+
+
+
