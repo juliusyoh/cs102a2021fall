37364756ce3823db6d78503ea3db4049b345dc7e
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-10-20 18:59:01 -0400

    lab4js4.js

---
+++ cs102/web-8/Lab4js4.js
@@ -0,0 +1,22 @@
+function toggle()
+{
+  object = document.getElementById("testme")
+ if( object.className == "visible" )
+  {
+    object.className = "invisible";
+  }
+  else
+  {
+    object.className = "visible";
+  }
+
+  object1 = document.getElementById("testme1")
+ if( object1.className == "invisible" )
+  {
+    object1.className = "visible";
+  }
+  else
+  {
+    object1.className = "invisible";
+  }
+}
