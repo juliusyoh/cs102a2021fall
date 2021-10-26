d6c41bd1240c8e053c690430087ebfd8125bf988
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-10-26 19:29:19 -0400

    chart.js

---
+++ cs102/web-8/chart.js
@@ -0,0 +1,14 @@
+function toggle()
+{
+    object = document.getElementById("testme")
+ if( object.className == "visible" )
+  {
+    object.className = "invisible";
+  }
+  else
+  {
+    object.className = "visible";
+  }
+}
+
+
