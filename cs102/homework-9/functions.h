40e1b3db42a489176f548886e12e4b2f57725a95
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-10-28 00:14:10 -0400

    functions.h

---
+++ cs102/homework-9/functions.h
@@ -0,0 +1,12 @@
+/*
+ * Julius Yoh
+ * CS102 Fall 2021
+ * Homework 9 - functions.h - Final Version
+ * October 27, 2021
+ */
+
+int string_length(char* string);
+int word_counter(char* string);
+int print_file( char* filename);
+
+
