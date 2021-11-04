e20a9f7fef71256a19656649bcfeda2f730a3c6c
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-11-03 20:22:30 -0400

    maze.c

---
+++ cs102/lab-10/lab5/maze.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "maze.h"
+
+/* Static global variable established  */
+static room_t rooms[10];
+static int location_x;
+
+/* FUNCTIONS */
+
+/* Returns  a copy of the global variable "location_x"  */
+int get_location_x() 
+{
+return location_x;
+
+}
+
+void set_location_x( int x )
+{
+location_x = x;
+}
+
+
+
+
+/* Establishes clear function */
+void clear_maze()
+ {
+int i = 0;
+for( i = 1; i < 10; i++ ) {
+    rooms[i].wall = 0;
+   }
+ }
+
+/* Establishes is_wall location */
+int is_wall( int x)
+{
+ return rooms[x].wall;
+}
+
+/* Establishes build_wall function */
+void build_wall(int x)
+ {
+rooms[x].wall = 1;
+ }
+
+/* Establishes clear_wall function */
+void clear_wall( int x)
+ {
+rooms[x].wall = 0;
+ }
+
+void print_maze()
+ {
+int i = 0;
+for( i = 1; i < 10; i++ ) {
+        if (is_wall(i) == 0){
+        fprintf(stdout, ". ");
+        }
+
+        if (is_wall(i) == 1){
+        fprintf(stdout, "##");
+        }
+
+        if (i == location_x){
+        fprintf(stdout, "x ");
+        }
+ }
+
+ }
+
+
