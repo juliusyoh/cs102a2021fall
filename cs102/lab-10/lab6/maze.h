e656a8cb23c786997d10951bcb98dbbd03f7d5a4
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-11-03 21:03:59 -0400

    maze.h

---
+++ cs102/lab-10/lab6/maze.h
@@ -0,0 +1,18 @@
+typedef struct { int wall; } room_t;
+void clear_maze();
+int is_wall( int x);
+void build_wall( int x);
+void clear_wall( int x);
+
+/* New Functions Lab 5 */
+void set_location_x( int x);
+int get_location_x();
+void print_maze();
+
+/* New Functions Lab 6 */
+void move_right();
+void move_left();
+
+
+
+
