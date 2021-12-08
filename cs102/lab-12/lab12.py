2850eaad7cb6df1d80a53303800be945dd14e9c6
juliusyoh <89947987+juliusyoh@users.noreply.github.com>
2021-12-08 18:46:11 -0500

    lab12.py

--- cs102/lab-12/lab12.py
+++ cs102/lab-12/lab12.py
@@ -13,18 +13,21 @@ def intersect (x1, y1, x2, y2, x3, y3, x4, y4):
 def edges( sx, sy, ex, ey ):
     return ( ( sx, sy, ex, sy), (ex, sy, ex, ey), (ex, ey, sx, ey), (sx, ey, sx, sy) )
 
-
-def northedge( sx, sy, ex, ey ):
-    return ( (sx, sy, ex, sy))
-
-def southedge( sx, sy, ex, ey ):
-    return ( (ex, sy, ex, ey))
-
-def eastedge( sx, sy, ex, ey ):
-    return ( (ex, ey, sx, ey))
-
-def westedge( sx, sy, ex, ey ):
-    return ( (sx, ey, sx, sy))
+def hits(sx1, sy1, ex1, ey1, sx2, sy2, ex2, ey2):
+    box1 = edges( sx1, sy1, ex1, ey1) 
+    box2 = edges(sx2, sy2, ex2, ey2)
+    list1 =[]
+    if intersectAny(box1[0], box2):
+        list1.append( 'N')
+    if intersectAny(box1[1], box2):
+        list1.append('E')
+    if intersectAny(box1[2], box2):
+        list1.append('S')
+    if intersectAny(box1[3], box2):
+        list1.append('W')
+    return list1
+    #USEbox1[0] for north edge
+    #Usebox1[1] for 
              
 def between( n, first, second ):
     if first <= n <= second:
@@ -40,41 +43,24 @@ def intersectAny( oneline, listoflines ):
         if intersect(*oneline, *line) != None:
             return True
     return False
-
-def hits(sx1, sy1, ex1, ey1, sx2, sy2, ex2, ey2):
-    list1 = []
-    box1 = (sx1, sy1, ex1, ey1)
-    box2 = (sx2, sy2, ex2, ey2)
-    northvalue = sx1
-    southvalue = sy1
-    eastvalue = ex1
-    westvalue = ey1
-    if intersectAny( northvalue, northedge) == True:
-        list1.append( 'N')
-    if intersectAny( southvalue, southedge) == True:
-        list1.append( 'S')
-    if intersectAny( eastvalue, eastedge) == True:
-        list1.append( 'E')
-    if intersectAny( westvalue, westedge)== True:
-        list1.append( 'W')
-    return list1
-    
     
 if __name__ == '__main__':
-    box1 = ( 1.5, 1, 9, 10 )
+    box1 = (1.5, 1, 9, 10)
     box2 = ( 0, 0, .5, .5 )
     box3 = ( 0, 0,  5,  5 )
     box4 = ( 0, 0, 11,  2 )
     box5 = ( 0, 0, 11, 11 )
     box6 = ( 3, 3,  4,  4 )
     box7 = ( 4, 0,  5,  5 )
+    hits( *(box1+box2) )
     print( hits( *(box1+box2) ) )
     print( hits( *(box1+box3) ) )
     print( hits( *(box1+box4) ) )
     print( hits( *(box1+box5) ) )
     print( hits( *(box1+box6) ) )
     print( hits( *(box1+box7) ) )
-  
+
     
+  
     
     
